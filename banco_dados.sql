eec2d5bc60ccbad2c7b2c6c74059b2b98eaff2b0
dalopess <111576661+dalopess@users.noreply.github.com>
2022-08-22 20:58:41 -0300

    Add files via upload

---
+++ banco_dados.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE base_dados; 
+
+-- para criar a base de dados novamente deu o seguinte erro: Error code: 1007. Can´t creat database "base_dados"; database exist
+
+
+DROP DATABASE IF EXISTS banco_dados;
+
+-- comando usado para deletar a base de dados
+
+-- Não deu erro, e sim um alerta. Roda mas com alerta
+
+
+
+
+
+
